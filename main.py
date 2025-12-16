1482384e870ef7f1f2eeacebff8a50fb3f1101c8
unsteadyapp <akanodia2029@kehillahstudent.org>
2025-12-16 10:12:24 -0800

    nameupdates

--- main.py
+++ main.py
@@ -1,86 +1,108 @@
 from simulator import robot, FORWARD, BACKWARD, STOP
 import time
-# TODO: Write your code here!
-# Use robot.motors() to move
-# Use robot.left_sonar() and robot.right_sonar() to sense obstacles
-grid = []
-x = 8
-
+import random
+import numpy as np
+class robotExtras():
+    pos = (0,0)
+    goal = None
+    direction = 0
+def onClick(coords):
+    if(coords>0):
+        robotExtras.goal = coords
+def rotateTo(angle):
+    angleInSeconds = (angle)/(0.98 * 60)
+    motorsAndWait(FORWARD,BACKWARD,abs(angleInSeconds))
+def AdvancedRotateTo(angle):
+    if(angle%360 == 0):
+        debugDirrection = None
+        angleInSeconds = 0
+    elif(angle%360<=180):
+        angleInSeconds = (angle)/(0.98 * 60)
+        debugDirrection = "right"
+        motorsAndWait(FORWARD,BACKWARD,abs(angleInSeconds))
+    else:
+        angleInSeconds = (180-(angle%180))/(0.98 * 60)
+        debugDirrection = "left"
+        motorsAndWait(BACKWARD,FORWARD,abs(angleInSeconds))
+    print(f"angle: {angle} converted to {angleInSeconds} while turning to the {debugDirrection}")
 def motorsAndWait(Left,Right,Seconds):
     robot.motors(left=Left,right=Right,seconds=Seconds)
-    time.sleep(abs(Seconds))
-import numpy as np
-for i in range(0,x):
-    grid.append("o" * int(x))
-where = (0,0)
-goal = (3,3)
-direction = 0
-def createMap():
-    mapping = grid.copy()
-    for i in range(len(grid)):
-        for j in range(len(grid[i])):
-            mapping[i] = mapping[i][:j] + str(np.abs(j - goal[0]) + np.abs(i-goal[1])) + mapping[i][j+1:]
-    return(mapping)
-def sonarScan():
-    if(robot.right_sonar()>0):
-        return(False)
+def forward(dist): #comment
+    """Distance to go: Float"""
+    print(dist)
+    print(speed_per_power_in_seconds)
+    print(dist/speed_per_power_in_seconds)
+    motorsAndWait(FORWARD,FORWARD,dist/speed_per_power_in_seconds)
+    print(f"done {dist/speed_per_power_in_seconds}")
+def sign(num):
+    """Number: Float
+    Returns : 1,-1"""
+    if(num>=0):
+        return(1)
     else:
-        return(True)
-def forward(helper):
-    # put whatever forward code here
-    motorsAndWait(FORWARD,FORWARD,2)
-    global where
-    where = helper
-    #where = (int(where[0]+np.round(np.sin((direction*np.pi)/180))),int(where[1]+np.round(np.cos((direction*np.pi)/180))))
-    print(where)
-end = False
-currentMap = createMap()
-print(currentMap)
-def angleCalculate(start,destination):
-    rotate(start-destination)
-def rotate(angle):
-    angleInSeconds = angle/(0.98 * 60)
-    # angle = 0.98 * 60 * seconds
-    #seconds = angle/(0.98 * 60)
-    motorsAndWait(BACKWARD,FORWARD,angleInSeconds)
-    global direction
-    direction = direction + angle
-where = [2,3]
-testing = (int(where[0]+np.round(np.sin((direction*np.pi)/180))),int(where[1]+np.round(np.cos((direction*np.pi)/180))))
-while end == False:
-    values = []
-    value = [100000,"imposible",""]
-    for p in [(1,0),(-1,0),(0,1),(0,-1)]:
-        whereTo = np.array(where) + np.array(p)
-        whereTo = [int(whereTo[0]),int(whereTo[1])]
-        print("the coords" + str(whereTo))
-        if(whereTo[0] < 8 and whereTo[1] < 8 and whereTo[0] > -1 and whereTo[1] > -1):
-            print("coord " + str((p[0],p[1])) + ". Value: " + str(value[0]) + ". Map" + str(int(currentMap[whereTo[1]][whereTo[0]])))
-            if(value[0] > int(currentMap[whereTo[1]][whereTo[0]])):
-                value[0] = int(currentMap[whereTo[1]][whereTo[0]])
-                value[2] = whereTo
-                if(p == (1,0)):
-                    value[1] = 0
-                    print(p)
-                elif(p==(0,1)):
-                    value[1] = 90
-                elif(p==(-1,0)):
-                    value[1] = 180
-                elif(p==(0,-1)):
-                    value[1] = 270
-                else:
-                    raise Exception("invalid list itterator value")
-                #value[1] = round(180 * np.atan2(p[1],p[0])/np.pi)
-                print("angle" + str(value[1]))
-            values.append([currentMap[whereTo[1]][whereTo[0]],direction])
-    if(value[1] == "imposible"):
-        print(value)
-        raise Exception("no space to move!")
-    angleCalculate(direction,value[1])
-    forward(value[2])
-    print("moved" + str(where))
-    currentMap = createMap()
-    if(where == [goal[0],goal[1]]):
-        end = True
-# When you're done, close the simulator
-robot.exit()
+        return(-1)
+#technicly 899 but as soon as it rotates it will crash
+#200 x 200
+# functionaly 100 * (2**1/2)
+inp = False
+while True:
+    print(robot.driver.x,robot.driver.y)
+    vector = (1000-(100 * (2**(1/2))),random.randint(-300,300))
+    vector = (1001-(100 * (2**(1/2))),0)
+    if(inp):
+        userInp = input("Left or Right").lower()
+        if(userInp == "left"):
+            print("not working right now, bye")
+            quit()
+        elif(userInp == "right"):
+            userInp = input("x coord").lower()
+            try:
+                if(userInp>0):
+                    vector[0] = userInp
+            except TypeError:
+                print("not a number, using default")
+        else:
+            print("that's not either")
+            quit()
+    print(robot.left_sonar())
+    print(vector)
+    signX = sign(vector[0]) == 1
+    signY = sign(vector[1]) == 1
+    if(signX and signY):
+        toAdd = 0
+        inverse = False
+        vector = (abs(vector[0]),abs(vector[1]))
+    elif(not(signX) and not(signY)):
+        toAdd = 180
+        inverse = False
+        vector = (abs(vector[0]),abs(vector[1]))
+    elif(not(signX) and signY):
+        toAdd = 90
+        inverse = True
+        vector = (abs(vector[1]),abs(vector[0]))
+    elif(signX and not(signY)):
+        toAdd = 270
+        inverse = True
+        vector = (abs(vector[1]),abs(vector[0]))
+    else:
+        raise Exception("Signs don't meet any conditions")
+    speed_per_power_in_seconds = 60
+    try:
+        angleInRad = (np.arctan(vector[1]/vector[0]))
+    except ZeroDivisionError:
+        angleInRad = np.pi
+    angleToRotateTo = toAdd + (angleInRad*180/np.pi)
+    AdvancedRotateTo(angleToRotateTo)
+    #distance = np.linalg.norm(vector)
+    distance = (vector[0]**2 + vector[1]**2)**(1/2)
+    sonarDist = robot.left_sonar()*10
+    if(sonarDist + (50 * (2**(1/2)))>distance):
+        raise Exception(f"Too close!, {sonarDist-(100 * (2**(1/2)))} is greater than {distance}")
+    else:
+        print(f"Sonar distance: {sonarDist}, while want to go = {distance}")
+    forward(distance)
+    rotateTo(180)
+    forward(distance)
+    AdvancedRotateTo((180 - (angleToRotateTo%360))%360)
+    if(robot.driver.x != 0 or robot.driver.y != 0 or robot.driver.heading != 0):
+        print("Not at origin: ",robot.driver.x,robot.driver.y,robot.driver.heading)
